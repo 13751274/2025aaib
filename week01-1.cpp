506d9e83db9bd4df3b01a5baed02838b48d64db4
13751274 <selina.chens2017@gmail.com>
2025-02-19 11:58:50 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,15 @@
+///week01-1.cpp瘋狂程設 兩數相加
+///file-new empty 開新的檔案
+///ctrl-s存檔成week01-1.cpp 要記得加.cpp
+///下周考這題 不用寫藍色的中文註解
+#include<stdio.h> ///檔名不能寫錯
+///stdio的意思 標準的standard input/output
+///有了stdio.h的外掛才能用scanf() printf()
+int main()
+{
+	int a, b;///整數a,b
+	scanf("%d", &a);///正課剛教 掃描讀東西
+	scanf("%d", &b);
+
+	printf("%d", a+b);///實習課有教 印東西
+}
